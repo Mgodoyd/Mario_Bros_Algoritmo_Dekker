2e4020b4794b96b40da29b96fe67c687fdbc784e
MARIO   GODOY <92261842+Mgodoyd@users.noreply.github.com>
2022-10-08 16:02:26 -0600

    edicion 2

--- Script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#check").addEventListener("click",function(){
-	let bulb = document.querySelector("#light");
-	if(this.checked)
-		bulb.classList.add("on");
-	else
-		bulb.classList.remove("on");
-});
